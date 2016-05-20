588f4348b1df4a2039b644b5dff87618ae86e197
Robin Suter <suter.robin@gmail.com>
2016-05-20 23:52:45 +0200

    implemented font-size-slider

--- scripts/font-size.js
+++ scripts/font-size.js
@@ -1,1 +1,9 @@
+"use strict";
 
+$(function () {
+
+    $('form#font-size-form > input').on('input', function () {
+        $("output#font-size").val($(this).val());
+        $("body").css("font-size", $(this).val() + "pt");
+    });
+});
